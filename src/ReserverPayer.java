4168d713ccc726a2ccb80e2acbd11c6612919d48
ausafelahi <ausafelahi8@gmail.com>
2026-01-17 15:15:35 +0500

    Hotel Reservation Major Changes

--- src/ReserverPayer.java
+++ src/ReserverPayer.java
@@ -4,14 +4,14 @@ public class ReserverPayer {
     private String id;
 
     private ReserverPayer(String creditCardDetails, String id) {
+        if (creditCardDetails == null || id == null) {
+            throw new IllegalArgumentException("Invalid reserver payer data");
+        }
         this.creditCardDetails = creditCardDetails;
         this.id = id;
     }
 
     public static ReserverPayer create(String creditCardDetails, String id) {
-        if (creditCardDetails == null || id == null) {
-            throw new IllegalArgumentException("Invalid reserver payer data");
-        }
         return new ReserverPayer(creditCardDetails, id);
     }
 }
